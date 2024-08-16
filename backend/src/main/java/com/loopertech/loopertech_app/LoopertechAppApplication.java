72f4406a147d0724e87c1676c1b35e2849389aef
Marvin <alejandrorodriguezreyes92@gmail.com>
2024-08-16 10:02:26 -0500

    Montaje del proyecto backend

---
+++ backend/src/main/java/com/loopertech/loopertech_app/LoopertechAppApplication.java
@@ -0,0 +1,13 @@
+package com.loopertech.loopertech_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoopertechAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoopertechAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/loopertech/loopertech_app/LoopertechAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loopertech.loopertech_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopertechAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
